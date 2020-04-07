b76449af565f19f0a465762c46f9346571417e59
benweizhu <benwei.zhu@exxonmobil.com>
2020-04-07 09:44:31 +0800

    init spring web

---
+++ src/main/java/me/zeph/example/spring/springredissessionexample/SpringRedisSessionExampleApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.example.spring.springredissessionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisSessionExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRedisSessionExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/zeph/example/spring/springredissessionexample/SpringRedisSessionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeph.example.spring.springredissessionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisSessionExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

2ec96a72571dafe39433502b7648869ae91f11d9
benweizhu <benwei.zhu@exxonmobil.com>
2020-04-07 12:23:53 +0800

    can use docker-compose to boot the application

--- src/main/java/me/zeph/example/spring/springredissessionexample/configuration/RedisConfiguration.java
+++ src/main/java/me/zeph/example/spring/springredissessionexample/configuration/RedisConfiguration.java
@@ -3,14 +3,21 @@ package me.zeph.example.spring.springredissessionexample.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
 public class RedisConfiguration {
 
+
   @ConfigurationProperties(prefix = "spring.redis")
   @Bean
-  public JedisConnectionFactory redisConnectionFactory() {
-    return new JedisConnectionFactory();
+  public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+    return new RedisStandaloneConfiguration();
+  }
+
+  @Bean
+  public JedisConnectionFactory redisConnectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration) {
+    return new JedisConnectionFactory(redisStandaloneConfiguration);
   }
 }

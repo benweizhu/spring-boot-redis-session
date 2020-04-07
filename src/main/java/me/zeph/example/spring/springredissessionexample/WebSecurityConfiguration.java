008ade84d36ebb44b166e33eda9223f5cd140ae5
benweizhu <benwei.zhu@exxonmobil.com>
2020-04-07 09:48:32 +0800

    add spring security and login for greeting page

---
+++ src/main/java/me/zeph/example/spring/springredissessionexample/WebSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package me.zeph.example.spring.springredissessionexample;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    UserDetails user = User.withUsername("username").password(encoder.encode("password")).roles("ADMIN").build();
+    auth.inMemoryAuthentication().withUser(user);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/**").hasRole("ADMIN")
+        .and()
+        .formLogin();
+  }
+}
